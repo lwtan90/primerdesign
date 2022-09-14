5c54c72ab59b34d5b6c16a39ce1ef2f8720693de
Wilson_Tan <lwtan@gmail.com>
2022-09-13 21:12:27 -0700

    Initialized class py file for primer.py

---
+++ lib/primer.py
@@ -0,0 +1,36 @@
+class Primer:
+
+    # constructor
+    def __init__(self,seq):
+        self.sequence = seq
+        self.length = len(seq)
+        self.gc = 0
+        self.tm = 0
+	self.returnInfo()
+
+    # return information about the primer
+    def returnInfo(self):
+        print("===================")
+        print("Information about primer " + self.getSequence())
+        print("GC Content: " + str(self.getGC()))
+        print("Primer Length: " + str(self.getLength()))
+        print("Melting Temp: " + str(self.getTm()))
+        print("====================")
+
+    # return length of the primer
+    def getLength(self):
+        return self.length
+
+    # return GC content of the primer
+    def getGC(self):
+        return self.gc
+
+    # return the sequence of the primer
+    def getSequence(self):
+        return self.sequence
+
+    # return the melting temperature of the primer
+    def getTm(self):
+        return self.tm
+
+p1 = Primer("ATTGCCC")
